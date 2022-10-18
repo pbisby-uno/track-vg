2292c8ac8837c30f69f957315d179dfa893a515e
pbisby-uno <philip.bisby@unosquare.com>
2022-10-18 11:25:54 +0100

    Update flyway migrations

--- migrations/local-data-seed/V1000.1__local_data_seed.sql
+++ migrations/local-data-seed/V1000.1__local_data_seed.sql
@@ -2,41 +2,20 @@ INSERT INTO public.users (id, username, password, enabled) VALUES (1, 'test_user
 INSERT INTO public.users (id, username, password, enabled) VALUES (2, 'test_user_2', 'pass2', true);
 INSERT INTO public.users (id, username, password, enabled) VALUES (3, 'test_user_3', 'pass3', false);
 
-INSERT INTO public.games (id, title) VALUES (1, 'Far Cry 6');
-INSERT INTO public.games (id, title) VALUES (2, 'Assassin''s Creed Odyssey');
-INSERT INTO public.games (id, title) VALUES (3, 'Fifa 23');
-INSERT INTO public.games (id, title) VALUES (4, 'Gran Turismo 7');
-INSERT INTO public.games (id, title) VALUES (5, 'The Last of Us Part 2');
+INSERT INTO public.titles (id, description) VALUES (1, 'Far Cry 6');
+INSERT INTO public.titles (id, description) VALUES (2, 'Assassin''s Creed Odyssey');
+INSERT INTO public.titles (id, description) VALUES (3, 'Fifa 23');
+INSERT INTO public.titles (id, description) VALUES (4, 'Gran Turismo 7');
+INSERT INTO public.titles (id, description) VALUES (5, 'The Last of Us Part 2');
 
-INSERT INTO public.reviews (id, review_text) VALUES (1, 'Loved it!');
+INSERT INTO public.games (id, platform_id, title_id, genre_id) VALUES(1, 6, 1, 5);
+INSERT INTO public.games (id, platform_id, title_id, genre_id) VALUES(2, 5, 2, 1);
+INSERT INTO public.games (id, platform_id, title_id, genre_id) VALUES(3, 6, 3, 3);
+INSERT INTO public.games (id, platform_id, title_id, genre_id) VALUES(4, 4, 4, 2);
+INSERT INTO public.games (id, platform_id, title_id, genre_id) VALUES(5, 3, 5, 10);
 
-INSERT INTO public.games_genres (game_id, genre_id) VALUES (1, 5);
-INSERT INTO public.games_genres (game_id, genre_id) VALUES (1, 8);
-INSERT INTO public.games_genres (game_id, genre_id) VALUES (2, 1);
-INSERT INTO public.games_genres (game_id, genre_id) VALUES (2, 8);
-INSERT INTO public.games_genres (game_id, genre_id) VALUES (3, 3);
-INSERT INTO public.games_genres (game_id, genre_id) VALUES (4, 2);
-INSERT INTO public.games_genres (game_id, genre_id) VALUES (5, 10);
-INSERT INTO public.games_genres (game_id, genre_id) VALUES (5, 11);
-
-INSERT INTO public.games_platforms (game_id, platform_id) VALUES (1, 6);
-INSERT INTO public.games_platforms (game_id, platform_id) VALUES (2, 5);
-INSERT INTO public.games_platforms (game_id, platform_id) VALUES (3, 6);
-INSERT INTO public.games_platforms (game_id, platform_id) VALUES (4, 4);
-INSERT INTO public.games_platforms (game_id, platform_id) VALUES (5, 3);
-
-INSERT INTO public.games_status (game_id, status_id) VALUES (1, 2);
-INSERT INTO public.games_status (game_id, status_id) VALUES (2, 1);
-INSERT INTO public.games_status (game_id, status_id) VALUES (3, 1);
-INSERT INTO public.games_status (game_id, status_id) VALUES (4, 2);
-INSERT INTO public.games_status (game_id, status_id) VALUES (5, 3);
-
-INSERT INTO public.users_games (user_id, game_id) VALUES (1, 1);
-INSERT INTO public.users_games (user_id, game_id) VALUES (1, 2);
-INSERT INTO public.users_games (user_id, game_id) VALUES (1, 3);
-INSERT INTO public.users_games (user_id, game_id) VALUES (2, 4);
-INSERT INTO public.users_games (user_id, game_id) VALUES (2, 5);
-
-INSERT INTO public.users_games_ratings (id, user_id, game_id, rating_id) VALUES (1, 2, 5, 9);
-
-INSERT INTO public.users_games_reviews (id, user_id, game_id, review_id) VALUES (1, 2, 5, 1);
+INSERT INTO public.users_games (user_id, game_id, status_id) VALUES(1, 1, 2);
+INSERT INTO public.users_games (user_id, game_id, status_id) VALUES(1, 2, 4);
+INSERT INTO public.users_games (user_id, game_id, status_id) VALUES(1, 3, 1);
+INSERT INTO public.users_games (user_id, game_id, status_id) VALUES(2, 4, 2);
+INSERT INTO public.users_games (user_id, game_id, status_id) VALUES(2, 5, 3);
